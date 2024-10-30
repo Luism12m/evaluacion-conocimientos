04510f4086ca6d823642abdd178e5efb726f97ca
luis zipa <luiscarloszipacogaria@gmail.com>
2024-10-30 00:18:53 +0000

    calculadora sin terminar

---
+++ calculadora.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main(int a , int b)
+{
+  
+  printf("la suma de a : %i\n, y b :%i\n es: %i\n" ,a ,b, suma(a,b);
+	 return (0);
+
+	 }
+    int suma(int x,int y)
+  {
+    
+   int sumar;
+   sumar = x + y;
+   
+   return (sumar); 
+  }
+
+}
